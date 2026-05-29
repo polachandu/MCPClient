44fed8f545ccf46e742a919f24929d54659071e5
polachandrahas@gmail.com <polachandrahas@gmail.com>
2026-05-29 14:50:03 -0700

    Implemented MCP Client

--- src/main/java/com/example/mcp/controller/McpClientController.java
+++ src/main/java/com/example/mcp/controller/McpClientController.java
@@ -2,10 +2,8 @@ package com.example.mcp.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -13,12 +11,15 @@ public class McpClientController {
 
     private final ChatClient chatClient;
 
-    public McpClientController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.defaultAdvisors(new SimpleLoggerAdvisor()).build();
+    public McpClientController(ChatClient.Builder chatClientBuilder, ToolCallbackProvider toolCallbackProvider) {
+        this.chatClient = chatClientBuilder
+                .defaultToolCallbacks(toolCallbackProvider)
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .build();
     }
 
     @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message){
-        return chatClient.prompt().user(message).call().content();
+    public String chat(@RequestHeader(value="username",required = false) String username, @RequestParam("message") String message){
+        return chatClient.prompt().user(message+"User name is : "+username).call().content();
     }
 }
